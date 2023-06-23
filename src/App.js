f1b6975ca43f9ee6e636e57aa80286ae9cfefade
AdityaBK1 <adityaitjob@gmail.com>
2023-06-23 16:33:22 +0530

    initial design

--- src/App.js
+++ src/App.js
@@ -1,125 +1,41 @@
 
 import './App.css';
 import React, { useState } from 'react';
-import {StudentCard}  from './Components/StudentCard';
-import { StateFulComp, StatelessComp } from './Components/statefulComponent';
-import Button from './Components/Button';
+import TopBar from './Components/TopBar';
+import Base from './BasePage/Base';
+import Dashboard from './Pages/Dashboard';
+import Profile from './Pages/Profile';
+
 
 function App() {
 
-const [acceptCount, setAcceptCount] = useState(0)
-  const studentsData = [
-    {
-      name:"Selvam",
-      batch:"B47WD",
-      education:"B.E",
-    },
-    {
-      name:"Sanjay",
-      batch:"B45WD",
-      education:"B.E",
-    },
-    {
-      name:"Baskar",
-      batch:"B43WD",
-      education:"BCA",
-    },
-    {
-      name:"Naveen",
-      batch:"B42WD",
-      education:"BSC",
-    },
-  ]
+
+const studentData = [
+  {
+  name : "Aditya",
+  age : 25
+  },
+  {
+    name : "ajay",
+    age : 23
+    }
+]
+const [data, setData] = useState(studentData)
   return (
 <div className="App">
-<div>
-  <h1>Accepted Students : {acceptCount}</h1>
-</div>
-<div className='card-block'>
-  {studentsData.map((stud,idx)=>(
-     <StudentCard
-     key={idx}
-     name={stud.name}
-     batch={stud.batch}
-     education={stud.education}
-     acceptCount={acceptCount}
-     setAcceptCount={setAcceptCount}
-     />
-  ))}
-</div>
-{/* <StateFulComp/>
-<StatelessComp
-name = {"sanjay"}
-/> */}
-
-<Button
- background={"crimson"}
- color="white"
- pd="15px"
- m="5px"
- wd="max-content"
- onClickFunc={()=>console.log("custom-btn clicked")}
->
-  Add
-</Button>
-
+  <Dashboard
+   data ={data}
+   setData ={setData}
+  />
 </div>
   ); 
 }
-// const props={} => 
-// props= {
-//   name:"",
-//    batch:"", 
-//    education:""}
-//destructuring 
-// const {name, batch, education} = props
+
 
 export default App;
 
 
 
+// useState() - day1
+// useEffect() -
 
-
-
-// function Welcome(){
-//   const welcomename = "guvi"
-//   return(
-//     React.createElement("div", {id:"welcome"}, 
-//     React.createElement("h1", {}, `Hi how are you ${welcomename}`))
-//   )
-// }
-//jsx -> javascript xml = (html with js)
-
-// functional component
-// function Greeting(){
-//   //normal js
-//   //function
-//   //varible
-//   const name = "sanjay"
-
-//   return (
-//     <div id="greeting" className='greeting-class'>
-//       <h1>Hi how are you {name}</h1>
-//     </div>
-//   )
-// }
-
-// virtual DOM
-// Components - In depth
-// Component life cycle
-// export types 
-// Stateful and stateless components
-// Reusable components 
-// Passing dynamic data to component
-// Introduction to Hook
-{/* <html>
-<body>
-  <div>
-    <h1></h1>
-    <div>
-      <button></button>
-      <p></p>
-    </div>
-  </div>
-</body>
-</html> */}
--- src/Components/StudentCard.js
+++ src/Components/StudentCard.js
@@ -1,43 +1,13 @@
-import { useState } from "react"
-import Button from "./Button"
-
- export  function StudentCard({name, batch, education, acceptCount, setAcceptCount}){
-    const [show, setShow] = useState(true)
-  
-    function handleAccept(){
-      setShow(!show)
-      setAcceptCount(acceptCount+1)
-    }
-  
-    function handleReject(){
-      setShow(!show)
-      setAcceptCount(acceptCount-1)
-      
-    }
+export default function StudentCard({data, setData}){
     return (
-      <div className='student-card'>
-        <img 
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ50haIo6IibH--JO8V4wJT_FGTLu3zECwg5EK_VQoT&s"
-         alt="name"/>
-        <h3>{name}</h3>
-        <p>{batch}</p>
-        <p>{education}</p>
-  
-      {
-      show ?   <Button 
-      customClass='accept-btn btn'
-      onClickFunc={handleAccept}
-        >Accept</Button> 
-        :    
-        <Button 
-        customClass='reject-btn btn'
-        onClickFunc={handleReject}
-        >Reject</Button>
-      }
-        
-  
-      </div>
+        <div>
+            {data.map((stud, index)=>(
+                <div key={index}>
+                   <h1>{stud.name}</h1>
+                   <h3>{stud.age}</h3>
+                </div>
+            ))}
+        </div>
     )
-  }
 
-
+}
